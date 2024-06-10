d9b057823e48585617df566c92dd6245bfaec12a
Renata Costa do Nascimento <renatacostarenatacostadonascimento@eaportal.org>
2024-06-10 01:46:01 +0000

    Feat:instalação e implantação do react-router-dom e do styled-components

---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+    homebackground: "#131518",
+    cardGradient: "radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%)",
+    textcolor: "#969FAD",
+    submitbuttoncolor: "#FC7614",
+    white: "#fff",
+    feedbackButtonColor: "#262E38",
+    feedbackButtonText: "#7C8798"
+}
